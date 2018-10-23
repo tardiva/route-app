c1d126e4efad425576f47dc432f625cd18d9578e
Irina Romanova <irina.tardiva@gmail.com>
2018-10-23 18:10:37 +0300

    Updates

---
+++ precache-manifest.abc940297d800ce97406c7ffa5960086.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d29e714552a08f1e4c6e",
+    "url": "/route-app/static/js/runtime~main.d29e7145.js"
+  },
+  {
+    "revision": "3e84fe27b5d595806d06",
+    "url": "/route-app/static/js/main.3e84fe27.chunk.js"
+  },
+  {
+    "revision": "45cd7b034a6956f7a7bc",
+    "url": "/route-app/static/js/1.45cd7b03.chunk.js"
+  },
+  {
+    "revision": "3e84fe27b5d595806d06",
+    "url": "/route-app/static/css/main.5fa0463c.chunk.css"
+  },
+  {
+    "revision": "6e5723fd0cd8dc9928d9822f780c4d8e",
+    "url": "/route-app/index.html"
+  }
+];
